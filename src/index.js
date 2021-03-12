a1c1becb19dc6562b49803b27f4b46b48006892c
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-03-12 19:42:08 +0200

    feat: implement search min, max, avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) {  
+      return 0;
+    }  
+    if (!Array.isArray(array)) {
+      array = Array.from(arguments);
+    }
+      let result = array[0];
+      array.forEach(element => {
+        if (element < result) {
+          result = element;
+        }
+      });
+        return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) {  
+      return 0;
+    }  
+    if (!Array.isArray(array)) {
+      array = Array.from(arguments);
+    }
+    let result = array[0];
+    array.forEach(element => {
+      if (element > result) {
+        result = element;
+      }
+    });
+    return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) {  
+    return 0;
+  }  
+  if (!Array.isArray(array)) {
+    array = Array.from(arguments);
+  }
+  let sum = array.reduce(function(result, value2) {
+    return result + value2;
+    }, 0);
+
+  return sum / array.length;
 }
